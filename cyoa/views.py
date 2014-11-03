7dd3d6c010c834d4b44f1489d01e46a923d97e57
Matt Makai <makaimc@gmail.com>
2014-11-03 05:32:55 -0800

    working up to tutorial-step-2

--- cyoa/views.py
+++ cyoa/views.py
@@ -1,7 +1,15 @@
-from flask import render_template, abort
+import cgi
+from flask import render_template, abort, request
 from jinja2 import TemplateNotFound
+from twilio import twiml
+from twilio.rest import TwilioRestClient
 
-from . import app
+from .config import TWILIO_NUMBER
+
+from . import app, redis_db
+
+client = TwilioRestClient()
+
 
 @app.route('/<presentation_name>/', methods=['GET'])
 def landing(presentation_name):
@@ -9,3 +17,14 @@ def landing(presentation_name):
         return render_template(presentation_name + '.html')
     except TemplateNotFound:
         abort(404)
+
+@app.route('/cyoa/twilio/webhook/', methods=['POST'])
+def twilio_callback():
+    to = request.form.get('To', '')
+    from_ = request.form.get('From', '')
+    message = request.form.get('Body', '').lower()
+    if to == TWILIO_NUMBER:
+        redis_db.incr(cgi.escape(message))
+    resp = twiml.Response()
+    resp.message("Thanks for your vote!")
+    return str(resp)
--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@ from gevent import monkey
 monkey.patch_all()
 
 import os
+import redis
 
 from cyoa import app, redis_db, socketio
 from flask.ext.script import Manager, Shell
@@ -16,6 +17,12 @@ manager.add_command("shell", Shell(make_context=make_shell_context))
 @manager.command
 def runserver():
     socketio.run(app, "0.0.0.0", port=5001)
+
+@manager.command
+def clear_redis():
+    redis_cli = redis.StrictRedis(host='localhost', port='6379', db='0')
+    redis_cli.delete('left')
+    redis_cli.delete('right')
 
 if __name__ == '__main__':
     manager.run()
